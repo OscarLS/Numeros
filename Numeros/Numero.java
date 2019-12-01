45f69d69d810713aeca72893b999a3d3b1d8c586
OscarLS <56656667+OscarLS@users.noreply.github.com>
2019-11-30 18:17:20 -0600

    Add files via upload

---
+++ Numeros/Numero.java
@@ -0,0 +1,29 @@
+/**
+ * Clase Numero.
+ * 
+ * @author López Santiago Oscar  
+ * @version 30/11/2019
+ */
+public class Numero
+{
+    //inicializar variables de instancia
+
+    /**
+     * Constructor de objetos de clase Contrasena
+     * @param
+     */
+
+    /**
+     * Método que indica si el numero es positivo, negativo o 0
+     * @parem num entero
+     */
+    public static void tiponum(double num)    {
+        //pon tu código aquí
+        if(num>0)
+            System.out.print(num+" es positivo");
+        else if(num<0)
+            System.out.print(num+" es negativo");
+        else System.out.print(num+" es cero");
+    }//Cierre del método
+
+} //Cierre de la clase
---
+++ Numeros/Prueba.java
@@ -0,0 +1,28 @@
+
+/**
+ * Clase cliente, para probar los metodos
+ * 
+ * @author López Santiago Oscar  
+ * @version 30/11/2019
+ */
+public class Prueba
+{
+    /**
+     * Método que compara las contraseñas 
+     * @parem args [] ignorar-
+     * @return void
+     */
+    public  static  void  main ( String [] args ) {
+        //pon tu código aquí
+        Numero.tiponum(10);
+        Numero.tiponum(5);
+        Numero.tiponum(-541);
+        Numero.tiponum(3156);
+        Numero.tiponum(0);
+        Numero.tiponum(85);
+        Numero.tiponum(-12);
+        Numero.tiponum(-785412);
+        Numero.tiponum(0);
+    }//Cierre del método
+
+} //Cierre de la clase
